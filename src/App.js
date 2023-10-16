bd542ebdb4ad51ae390d2185d41e10d777590132
shishir <eftekharAhmedshishir@gmail.com>
2023-10-16 18:08:23 +0600

    footer complite

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Footer from './Components/Footer';
+import KnowMore from './Components/Know More/KnowMore';
+
+function App() {
+  return (
+    <div className="">
+      <KnowMore></KnowMore>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
